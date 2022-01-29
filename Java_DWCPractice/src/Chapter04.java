682ddbb8bd6e328f491ed514a803b1c4bb41fb3f
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-01-29 22:14:39 +0900

    First Commit

---
+++ Java_DWCPractice/src/Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String str1 = "Yu";
+    final int num1 = 8;
+    System.out.println(str1);
+    str1 = "Shinomiya";
+    System.out.println(str1);
+    System.out.println(num1);
+  }
+
+}
