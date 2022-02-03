c034dd7c4ff0698de8f54752291999bcec3673e4
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-02-04 00:00:22 +0900

    2/3practice

---
+++ Java_DWCPractice/src/classMethod/Chapter09.java
@@ -0,0 +1,15 @@
+package classMethod;
+
+import ClassMethod.car.Car
+
+public class Chapter09 {
+  double fuelCost = 12.5;
+    // 残量（L）
+    double fuelAmount = 55.0;
+
+    Car car = new Car(fuelCost, fuelAmount);
+
+    car.move(20);
+    System.out.println("残量は、" + car.getFuelAmount() + "です");
+  }
+}
