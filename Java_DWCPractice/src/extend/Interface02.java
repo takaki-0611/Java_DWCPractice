905244def9ec8370a7b9be9e9ebb1023c764bcee
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-02-15 22:51:05 +0900

    2/15practice

---
+++ Java_DWCPractice/src/extend/Interface02.java
@@ -0,0 +1,10 @@
+package extend;
+
+import extend.phone.Phone;
+
+public class Interface02 {
+  public static void main(String[] args) {
+    Phone Phone = new Phone("000-1111-2222");
+    Phone.powerOff();
+  }
+}
